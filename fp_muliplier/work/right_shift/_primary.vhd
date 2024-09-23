b493e5efd7537d18be26cd66f1e70667ae74ce44
Chandeepa Janith Peiris <chandeepajanith2000@gmail.com>
2024-09-23 16:28:25 +0530

    right shifter for multiplier

---
+++ fp_muliplier/work/right_shift/_primary.vhd
@@ -0,0 +1,9 @@
+library verilog;
+use verilog.vl_types.all;
+entity right_shift is
+    port(
+        \in\            : in     vl_logic_vector(49 downto 0);
+        shift_amount    : in     vl_logic_vector(7 downto 0);
+        \out\           : out    vl_logic_vector(22 downto 0)
+    );
+end right_shift;
---
+++ fp_muliplier/work/right_shift_tb/_primary.vhd
@@ -0,0 +1,4 @@
+library verilog;
+use verilog.vl_types.all;
+entity right_shift_tb is
+end right_shift_tb;
